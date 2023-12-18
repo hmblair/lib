11a4ec64dbbdfed875579ff906f3472985a90c55
Hamish B <hmblair@stanford.edu>
2023-12-18 15:56:16 -0800

    The forward method now no longer returns the attention weights; use get_attn_weights instead

--- models/bedrock/attention.py
+++ models/bedrock/attention.py
@@ -2,8 +2,8 @@
 
 import torch
 import torch.nn as nn
-from typing import Callable, Optional, Union
-from .abstract_models import BaseModel, WeightInitialisationMetaClass
+from typing import Callable, Optional, Union, Any
+from .abstract_models import BaseModel, WeightInitialisationMetaClass, patch_and_register_layer_hooks, HookContextManager
 import fm
 
 # will this return the self-attention weights twice?
@@ -13,7 +13,10 @@ AttentiveModule = (
     # nn.TransformerDecoderLayer,
     fm.multihead_attention.MultiheadAttention,
 )
-def patch_attn_to_return_weights(m : nn.MultiheadAttention) -> None:
+def patch_attn_to_return_weights(
+        m : nn.MultiheadAttention,
+        average_attn_weights : Optional[bool] = None,
+        ) -> None:
     """
     Force the attention module to return the attention weights.
 
@@ -21,34 +24,70 @@ def patch_attn_to_return_weights(m : nn.MultiheadAttention) -> None:
     ----------
     m (nn.MultiheadAttention): 
         The attention module.
+    average_attn_weights (bool | None):
+        Whether to average the attention weights across the heads. If None,
+        then the default setting of the attention module is used. The reason 
+        to use None is that some attention modules do not have this setting.
     """
     forward_orig = m.forward
 
     def wrap(*args, **kwargs):
         kwargs["need_weights"] = True
-        # kwargs["average_attn_weights"] = False
+        if average_attn_weights is not None:
+            kwargs["average_attn_weights"] = average_attn_weights
         return forward_orig(*args, **kwargs)
 
     m.forward = wrap
 
 
-class SaveAttentionWeights:
+class AttentionWeightsHook(HookContextManager):
     """
     A hook that saves the attention weights of a transformer or attention layer.
     """
-    def __init__(self):
+    def __init__(self, model):
+        super().__init__(
+            module=model,
+            layer_type=AttentiveModule,
+            patch=patch_attn_to_return_weights,
+            )
+        # a list to store the attention weights
         self.outputs = []
 
-    def __call__(self, module, module_in, module_out):
-        self.outputs.append(module_out[1])
 
-    def clear(self):
-        self.outputs = []
+    def __call__(
+            self, 
+            module : nn.Module, 
+            module_in : Any, 
+            module_out : Any,
+            ) -> None:
+        """
+        Save the attention weights of the given module.
+        
+        Parameters:
+        ----------
+        module (nn.Module):
+            The module whose attention weights are to be saved.
+        module_in (Any):
+            The input to the module.
+        module_out (Any):
+            The output of the module.
+        """
+        self.outputs.append(
+            module_out[1].detach()
+            )
 
-    def pop_weights(self):
-        out = torch.stack(self.outputs, dim=1)
-        self.clear()
-        return out
+
+    def get_weights(self) -> torch.Tensor:
+        """
+        Get the attention weights of the model, as saved by the hook.
+
+        Returns:
+        --------
+        torch.Tensor: 
+            The attention weights of the model, of shape 
+            (*, n_layers, n_heads, seq_len, seq_len).
+        """
+        return torch.stack(self.outputs, dim=1)
     
 
 
@@ -85,68 +124,50 @@ def patch_forward_to_return_attn_weights(m : nn.Module):
 
 
 
-class AttentionWeightMetaClass(WeightInitialisationMetaClass):
-    """
-    A metaclass that automatically calls the _weight_init() method of a class
-    after all child classes are initialized. This is useful for initializing
-    the weights of a model after it is initialized.
+# class AttentionWeightMetaClass(WeightInitialisationMetaClass):
+#     """
+#     A metaclass that automatically calls the _weight_init() method of a class
+#     after all child classes are initialized. This is useful for initializing
+#     the weights of a model after it is initialized.
 
-    Any class that inherits from this metaclass must implement the _weight_init()
-    method, and have a save_attention_weights attribute.
+#     Any class that inherits from this metaclass must implement the _weight_init()
+#     method, and have a save_attention_weights attribute.
 
-    Inherits:
-    --------
-    type: 
-        A metaclass for creating classes.
-    """
-    def __call__(cls, *args, **kwargs):
-        # create an instance of the class using the __call__ method of the class
-        assert issubclass(cls, BaseAttentionModel), 'The class must inherit from BaseAttentionModel'
-        obj = WeightInitialisationMetaClass.__call__(cls, *args, **kwargs)
+#     Inherits:
+#     --------
+#     type: 
+#         A metaclass for creating classes.
+#     """
+#     def __call__(cls, *args, **kwargs):
+#         # create an instance of the class using the __call__ method of the class
+#         assert issubclass(cls, BaseAttentionModel), 'The class must inherit from BaseAttentionModel'
+#         obj = WeightInitialisationMetaClass.__call__(cls, *args, **kwargs)
 
-        if hasattr(obj, 'attention_weights'):
-            # patch the model so that the attention layers return the attention weights
-            # and register a hook to save the attention weights
-            obj.patch_and_register_layer_hooks(
-                layer_type=AttentiveModule,
-                hook=obj.attention_weights,
-                transform=None,
-                patch=patch_attn_to_return_weights,
-            )
+#         if hasattr(obj, 'attention_weights'):
+#             # patch the model so that the attention layers return the attention weights
+#             # and register a hook to save the attention weights
+#             obj.patch_and_register_layer_hooks(
+#                 layer_type=AttentiveModule,
+#                 hook=obj.attention_weights,
+#                 transform=None,
+#                 patch=patch_attn_to_return_weights,
+#             )
 
-            # patch the model so that the forward pass returns the attention weights
-            # patch_forward_to_return_attn_weights(obj)
+#             # patch the model so that the forward pass returns the attention weights
+#             # patch_forward_to_return_attn_weights(obj)
 
-        return obj
+#         return obj
 
 
-class BaseAttentionModel(BaseModel, metaclass=AttentionWeightMetaClass):
-    def __init__(
-            self, 
-            save_attn_weights : bool = False,
-            ):
+class BaseAttentionModel(BaseModel):
+    def __init__(self):
         super().__init__()
-        self.save_attn_weights = save_attn_weights
-        # self.attention_weights = SaveAttentionWeights()
-
-    
-    def save_attn(self, save_attn_weights : bool = True) -> None:
-        """
-        Toggle whether to save the attention weights of the model during the
-        forward pass.
-
-        Parameters:
-        ----------
-        save_attn_weights (bool): 
-            Whether to save the attention weights of the model.
-            Defaults to True.
-        """
-        self.save_attn_weights = save_attn_weights
 
     
     def get_attn_weights(self, x : torch.Tensor) -> torch.Tensor:
         """
-        Get the attention weights of the given input.
+        Get the attention weights of each layer of the model when the given 
+        input is passed through the model.
 
         Parameters:
         ----------
@@ -159,8 +180,7 @@ class BaseAttentionModel(BaseModel, metaclass=AttentionWeightMetaClass):
             The attention weights of the model, of shape 
             (*, n_layers, n_heads, seq_len, seq_len)
         """
-        if not self.save_attn_weights:
-            raise AttributeError('Please set save_attn_weights to True before calling this method.')
-        self(x)
-        return self.attention_weights.pop_weights()
+        with AttentionWeightsHook(self) as hook:
+            self(x)
+        return hook.get_weights()
 
