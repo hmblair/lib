d2c74c43ff88ec4ccd8cdfe0c875de38f42668b5
Hamish B <hmblair@stanford.edu>
2024-01-07 18:06:23 -0800

    A scheduler for unfreezing layers progressively

---
+++ finetuning/scheduler.py
@@ -0,0 +1,41 @@
+# scheduler.py
+
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import BaseFinetuning
+from pytorch_lightning.utilities.model_summary import summarize
+
+class FineTuningScheduler(BaseFinetuning):
+    def __init__(
+            self, 
+            layers_to_unfreeze : list[int],
+            unfreeze_rate : int, 
+            pt_model : str = 'pt_model',
+            ) -> None:
+        self.unfreeze_rate = unfreeze_rate
+        self.pt_model = pt_model
+        self._unfreeze_iter = layers_to_unfreeze
+    
+
+    def freeze_before_training(self, pl_module: pl.LightningModule) -> None:
+        if self.unfreeze_rate == 0:
+            return
+        try:
+            getattr(pl_module, self.pt_model).freeze()
+        except Exception as e:
+            raise ValueError(
+                f'Cannot find {self.pt_model} in the LightningModule. '
+                'Please check the name of the attribute.'
+            ) from e
+    
+
+    def finetune_function(
+            self, 
+            pl_module: pl.LightningModule, 
+            epoch: int, 
+            optimizer: torch.optim.Optimizer,
+            ) -> None:
+        if epoch > 0 and epoch % self.unfreeze_rate == 0:
+            i = next(self._unfreeze_iter)
+            getattr(pl_module, self.pt_model)[i].requires_grad_(True)
+            print(summarize(pl_module))
