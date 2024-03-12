2db38a9ea7b0736f7ada38e345a06d3806448dd7
Hamish B <hmblair@stanford.edu>
2024-03-12 16:44:18 -0700

    Debugging cleanup

--- training/modules.py
+++ training/modules.py
@@ -448,8 +448,6 @@ class DenoisingDiffusionModule(pl.LightningModule):
         # sample standard normal noise
         z = torch.randn_like(x)
 
-        print(x.device, z.device, self.alpha_bar[t].device)
-
         # apply the forward diffusion process
         diffuse_x = x * torch.sqrt(self.alpha_bar[t]) + z * torch.sqrt(1 - self.alpha_bar[t])
 
