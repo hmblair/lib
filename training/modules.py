f6025180f667f107e471b6f83202082df9ae7793
Hamish B <hmblair@stanford.edu>
2024-03-23 23:58:53 -0700

    Debugging

--- training/modules.py
+++ training/modules.py
@@ -537,9 +537,14 @@ class DenoisingDiffusionModule(pl.LightningModule):
 
         # apply the reverse diffusion process
         for t in trange(len(self.betas) - 1, -1, -1):
-            print(torch.cuda.memory_allocated())
             x = self.reverse_diffusion(x, t, **kwargs)
-
+            import gc
+            for obj in gc.get_objects():
+                try:
+                    if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+                        print(type(obj), obj.size())
+                except:
+                    pass
         return x
     
 
